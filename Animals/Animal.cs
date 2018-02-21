f4eb7d6c0e1de4cf300c66f9734992f29a392edd
PeaceAgent <shahobjon33@gmail.com>
2018-02-20 19:51:18 -0500

    Classes

---
+++ Animals/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected virtual int NumberofLegs {
+            get {
+                return 0;
+            }
+        }
+
+        protected virtual int  PairsofEyes { 
+        
+            get {
+                return 0;
+            }
+        }
+
+        public int Age { get; set; }
+
+        public Animal (int age)
+        {
+            this.Age = age;
+            Console.WriteLine("Animal created");
+        }
+        
+        public void PrintLegsAndEyes()
+        {
+            Console.WriteLine(String.Format("I have {0} legs and {1} eyes", this.NumberofLegs, this.PairsofEyes * 2));
+        }
+
+        public void PrintAge()
+        {
+            Console.WriteLine(String.Format("I have {0} years old", this.Age));
+        }
+
+    }
+
+}
---
+++ Animals/Mammal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Mammal: Animal
+    {
+        protected override int PairsofEyes { get { return 1; } }
+        public bool isPregnant { get; set; }
+        private void Init(bool isPregnant)
+        {
+            this.isPregnant = isPregnant;
+            Console.WriteLine("Mammal created.");
+
+        }
+
+        public Mammal(int age ) : base(age)
+        {
+            this.Init(false);
+        }
+
+        public Mammal(int age, bool isPregnant) :base(age)
+        {
+            this.Init(isPregnant);
+        }
+    }
+}
