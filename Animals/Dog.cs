46d7ca84e7e5337679453c2fef7c842a2c157a4a
PeaceAgent <shahobjon33@gmail.com>
2018-02-20 20:05:25 -0500

    new change :)

---
+++ Animals/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Dog : DomesticMammal
+    {
+        protected override int NumberofLegs { get { return 4; } }
+        public virtual string Breed { get { return "Just a dog"; } }
+        public virtual string BreedFamily { get { return "Dog"; } }
+
+        private void Init ()
+        {
+            Console.WriteLine("Dog created");
+        }
+        public Dog(string name, int age, string favoriteToy, bool isPregnant) : base(name, age, favoriteToy, isPregnant)
+        { 
+            this.Init();
+
+        }
+        
+        public Dog(string name, int age, string favoriteToy) : base(name, age, favoriteToy)
+        { 
+            this.Init();
+
+        }
+    }
+}
